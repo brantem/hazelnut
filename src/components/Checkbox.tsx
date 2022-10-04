5b86b4182814263bb9dab9cd26285d7b76e3bbac
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2022-10-04 17:44:42 +0700

    Add `color` props to Checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,9 +2,10 @@ import { InputHTMLAttributes } from 'react';
 
 type CheckboxProps = React.DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
   label: string;
+  color?: string;
 };
 
-const Checkbox = ({ label, name, ...props }: CheckboxProps) => {
+const Checkbox = ({ label, name, color = 'neutral', ...props }: CheckboxProps) => {
   return (
     <div className="w-full flex items-center">
       <input
@@ -12,9 +13,9 @@ const Checkbox = ({ label, name, ...props }: CheckboxProps) => {
         name={name}
         type="checkbox"
         {...props}
-        className="h-4 w-4 rounded border-gray-300 text-neutral-600 focus:ring-neutral-500"
+        className={`h-4 w-4 rounded border-${color}-300 text-${color}-600 focus:ring-${color}-500`}
       />
-      <label htmlFor={name} className="font-medium text-gray-700 ml-3 flex-1 truncate">
+      <label htmlFor={name} className="font-medium text-neutral-700 ml-3 flex-1 truncate">
         {label}
       </label>
     </div>
