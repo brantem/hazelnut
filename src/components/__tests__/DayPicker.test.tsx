fe494a55fa4fa77cd6db9b73f68fb7a6a46ef760
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2022-10-05 15:12:12 +0700

    Improve DayPicker test

--- src/components/__tests__/DayPicker.test.tsx
+++ src/components/__tests__/DayPicker.test.tsx
@@ -1,34 +1,57 @@
-import { render, screen, act } from '@testing-library/react';
+import { render, screen, act, fireEvent, waitFor } from '@testing-library/react';
 
 import '@testing-library/jest-dom';
 
 import DayPicker from 'components/DayPicker';
 
 describe('DayPicker', () => {
-  it('should render successfully', () => {
+  it('should be clickable', () => {
     const onChange = vi.fn(() => {});
-    const { container, rerender } = render(<DayPicker value={[]} onChange={onChange} />);
+    const { rerender } = render(<DayPicker value={[]} onChange={onChange} />);
 
-    expect(container).toMatchSnapshot();
-    expect(container.querySelector('[aria-checked="true"]')).not.toBeInTheDocument();
     const option = screen.getByTestId('day-picker-option-monday');
+    expect(option).toHaveAttribute('aria-checked', 'false');
     act(() => option.click());
     expect(onChange).toHaveBeenCalledWith(['MONDAY']);
     rerender(<DayPicker value={['MONDAY']} onChange={() => {}} />);
     expect(option).toHaveAttribute('aria-checked', 'true');
-    expect(container).toMatchSnapshot();
-
-    // TODO: test space
   });
 
-  it("shouldn't be selectable when disabled", () => {
+  it('should be selectable', async () => {
     const onChange = vi.fn(() => {});
-    const { container } = render(<DayPicker value={[]} onChange={onChange} isDisabled />);
+    const { rerender } = render(<DayPicker value={[]} onChange={onChange} />);
 
-    expect(container).toMatchSnapshot();
-    act(() => screen.getByTestId('day-picker-option-monday').click());
+    const option = screen.getByTestId('day-picker-option-monday');
+    expect(option).toHaveAttribute('aria-checked', 'false');
+    fireEvent.keyDown(option, { code: 'Space' });
+    await waitFor(() => new Promise((res) => setTimeout(res, 0)));
+    expect(onChange).toHaveBeenCalledWith(['MONDAY']);
+    rerender(<DayPicker value={['MONDAY']} onChange={() => {}} />);
+    expect(option).toHaveAttribute('aria-checked', 'true');
+  });
+
+  it('should be able to unselect', async () => {
+    const onChange = vi.fn(() => {});
+    const { rerender } = render(<DayPicker value={['MONDAY']} onChange={onChange} />);
+
+    const option = screen.getByTestId('day-picker-option-monday');
+    expect(option).toHaveAttribute('aria-checked', 'true');
+    act(() => option.click());
+    expect(onChange).toHaveBeenCalledWith([]);
+    rerender(<DayPicker value={[]} onChange={() => {}} />);
+    expect(option).toHaveAttribute('aria-checked', 'false');
+  });
+
+  it("shouldn't be selectable when disabled", async () => {
+    const onChange = vi.fn(() => {});
+    render(<DayPicker value={[]} onChange={onChange} isDisabled />);
+
+    const option = screen.getByTestId('day-picker-option-monday');
+    act(() => option.click());
     expect(onChange).not.toHaveBeenCalled();
 
-    // TODO: test space
+    fireEvent.keyDown(option, { code: 'Space' });
+    await waitFor(() => new Promise((res) => setTimeout(res, 0)));
+    expect(onChange).not.toHaveBeenCalled();
   });
 });
