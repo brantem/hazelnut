946861fb220c23f294b00c7c989a2e72417645bd
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2022-10-19 21:05:09 +0700

    Add next-pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import { useLoadStore } from 'lib/stores';
 
@@ -7,7 +8,28 @@ import 'styles/globals.css';
 function MyApp({ Component, pageProps }: AppProps) {
   useLoadStore();
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Hazelnut" />
+        <meta name="keywords" content="Hazelnut" />
+        <title>Hazelnut</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link href="/icons/icon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/icons/icon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
