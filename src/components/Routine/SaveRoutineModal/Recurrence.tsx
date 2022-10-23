2ae658d45e30409b155e2fff2b539b0a38296cb6
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2022-10-23 12:34:09 +0700

    Fix UI

--- src/components/Routine/SaveRoutineModal/Recurrence.tsx
+++ src/components/Routine/SaveRoutineModal/Recurrence.tsx
@@ -17,22 +17,15 @@ const Recurrence = ({ value, onChange, isDisabled, showNext }: RecurrenceProps) 
   return (
     <div data-testid="repeat-every" className="space-y-2">
       <div className="flex w-full items-center space-x-3">
-        <span className="flex-shrink-0 text-sm">From</span>
-        <Input
-          type="date"
-          data-testid="recurrence-startAt"
-          name="recurrence.startAt"
-          value={dayjs(value.startAt).format('YYYY-MM-DD')}
-          onChange={(e) => {
-            const startAt = e.target.value ? dayjs(e.target.value) : dayjs();
-            onChange({ ...value, startAt: startAt.startOf('day').valueOf() });
-          }}
-          disabled={isDisabled}
-          className="text-sm"
-          required
-        />
-        <span className="flex-shrink-0 text-sm">repeat every</span>
-        <div className="w-14 flex-shrink-0">
+        <span className="flex-shrink-0 text-sm text-neutral-700">From</span>
+
+        <span className="flex-grow-1 flex-auto flex-shrink-0 rounded-md border border-neutral-300 bg-neutral-100 p-2 text-center text-sm">
+          {dayjs(value.startAt).format('D MMM YYYY')}
+        </span>
+
+        <span className="flex-shrink-0 text-sm text-neutral-700">repeat every</span>
+
+        <div className="w-10">
           <Input
             type="number"
             data-testid="recurrence-interval"
@@ -41,10 +34,11 @@ const Recurrence = ({ value, onChange, isDisabled, showNext }: RecurrenceProps) 
             onChange={(e) => onChange({ ...value, interval: parseInt(e.target.value) })}
             disabled={isDisabled}
             min="1"
-            className="text-center text-sm"
+            className="px-2 text-center text-sm"
             required
           />
         </div>
+
         <select
           data-testid="recurrence-frequency"
           name="recurrence.frequency"
@@ -59,7 +53,7 @@ const Recurrence = ({ value, onChange, isDisabled, showNext }: RecurrenceProps) 
           }}
           disabled={isDisabled}
           required
-          className="block h-10 w-24 rounded-md border-neutral-300 bg-white text-sm focus:border-neutral-500 focus:ring-neutral-500 disabled:border-neutral-200 disabled:bg-neutral-100 disabled:text-neutral-500"
+          className="block h-10 w-[94px] rounded-md border-neutral-300 bg-white pl-2 text-sm focus:border-neutral-500 focus:ring-neutral-500 disabled:border-neutral-200 disabled:bg-neutral-100 disabled:text-neutral-500"
         >
           <option value="DAILY">{value.interval > 1 ? 'days' : 'day'}</option>
           <option value="WEEKLY">{value.interval > 1 ? 'weeks' : 'week'}</option>
